c125ed989f5d174287ff67e2386f7924f8c97d74
skmn3 <sangjin170202@naver.com>
2021-12-04 22:53:07 +0900

    [UPDATE] createPost routes

--- routes/createPost.js
+++ routes/createPost.js
@@ -9,12 +9,22 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => { 
         cb(null, Date.now() + "-" + file.originalname);   
     }, 
+    filefilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        if(ext !== "jpeg" || "jpg" || "png" || "gif" || "pdf" || "svg") {
+            return cb(null, false)
+        }
+        cb(null, true)
+      },
+      limit: {
+        filesize: 1024 * 1024 * 20
+      },
 });
          
 const upload = multer({
     storage: storage,
 });
         
-router.post("/", verifyToken, upload.single('image'), controller.createPost);
+router.post("/", verifyToken, upload.array('image'), controller.createPost);
 
 module.exports = router;
