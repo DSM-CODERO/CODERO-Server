3f5db6a4abf0bc61a4e6c8fb1b6fd4c429cbd5df
skmn3 <sangjin170202@naver.com>
2021-12-04 20:50:10 +0900

    [UPDATE] comment모델

--- models/comment.js
+++ models/comment.js
@@ -1,57 +1,52 @@
 const Sequelize = require('sequelize');
 
-module.exports = class Comment extends Sequelize.Model {
-    static init(sequelize) {
-      return super.init({
-        comment_id: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-        },
-        board_id: {
-          type: Sequelize.INTEGER,
-        },
-        user_id: {
-          type: Sequelize.INTEGER,
-        },
-        context: {
-          type: Sequelize.TEXT,
-          allowNull: true,
-        },
-        reply_comment_id: {
-          type: Sequelize.INTEGER,
-          allowNull: true,
-        },
-        picture: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        // isDeleted: {
-        //   type: Sequelize.BOOLEAN,
-        // },
-        created_at: {
-          type: Sequelize.DATE,
-          allowNull: false,
-          defaultValue: Sequelize.NOW,
-        },
-        // updated_at: {
-        //   type: Sequelize.DATE,
-        //   allowNull: false,
-        // },
-      }, {
-        sequelize,
-        underscored: false,
-        modelName: 'Comment',
-        tableName: 'comments',
-        timestamps: false,
-        paranoid: false, 
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci',
-      });
+module.exports = (sequelize, Datatypes) => {
+  return sequelize.define("comment", {
+    comment_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    board_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+    },
+    context: {
+      type: Datatypes.TEXT,
+      allowNull: true,
+    },
+    reply_comment_id: {
+      type: Datatypes.INTEGER,
+      allowNull: true,
+    },
+    picture: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    isDeleted: {
+      type: Datatypes.BOOLEAN,
+    },
+    created_at: {
+      type: Datatypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW
+    },
+    updated_at: {
+      type: Datatypes.DATE,
+      allowNull: false,
     }
-
-    static associate(db) {
-      db.Comment.belongsTo(db.Board, { foreignKey: 'board_id', targetKey: 'board_id' });
-      db.Comment.belongsTo(db.User, { foreignKey: 'user_id', targetKey: 'user_id' });
-    }
-}
+  },
+  {
+    sequelize,
+    timestamps: false,
+    modelName: 'Comment',
+    tableName: 'comments',
+    paranoid: false,
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_general_ci',
+  })
+};
