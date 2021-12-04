5462a227d43e83c3ea3aea6515792f4e8e5670ad
yebin <yebin5127@gmail.com>
2021-12-05 02:02:20 +0900

    [CREATE] comment controllers
    아직 수정 안 한 파일

---
+++ controllers/comment.controller.js
@@ -0,0 +1,112 @@
+const { Comment } = require("../models");
+
+const GetComment = async(req, res) => {
+    const BoardID = req.decoded.board_id;
+    
+    try{
+        const comment = await Comment.findAll({
+            where: {
+                board_id : BoardID
+            },
+        });
+        res.status(200).json(comment);
+    } catch(err){
+        res.status(404).json({
+            message: err.message
+        });
+    }
+}
+
+const Commentcreate = async(req, res) => {
+    const UserID = req.decoded.user_id;
+    const BoardID = req.decoded.board_id;
+    const nickName = req.decoded.nickname;
+    const Image = req.body;
+    const { context } = req.body;
+
+    try{
+        const result = await Comment.findOne({
+            where : {
+                board_id : BoardID
+            },
+        });
+        await result.create({
+            user_id : UserID,
+            nickname : nickName,
+            context : context,
+            picture: Image.path
+        });
+        res.status(200).json({ 
+            result,
+            message: "Comment Created successfully" 
+        });
+    } catch(err){
+        res.status(404).json({
+            message: "Failed Created Comment"
+        });
+        console.error(err)
+    }
+}
+
+
+const Commentupdate = async(req, res) => {
+    const UserID = req.decoded.user_id;
+    const CommentID = req.body;
+    const { context } = req.body;
+
+    try{
+        const result = await Comment.findOne({
+            where : {
+                comment_id : CommentID
+            },
+        });
+        if(result.user_id !== UserID) {
+            res.status(400).json({
+                message: "Failed Updated Comment"
+            });
+        } else await result.update({
+                context : context,
+            });
+            res.status(200).json({
+                message: "Comment Updated successfully"
+            });
+    } catch(err) {
+        res.status(403).json({
+            message: "Failed Updated Comment"
+        });
+    }
+} 
+
+const Commentdelete = async(req, res) => {
+    const UserID = req.decoded.user_id;
+    const CommentID = req.body;
+
+    try{
+        const result = await Comment.findOne({
+            where : {
+                comment_id : CommentID 
+            },
+        });
+        if(result.user_id !== UserID) {
+            res.status(400).json({
+                message: "Comments made by other users cannot be deleted."
+            });
+        } else {
+            await result.destroy();
+            res.status(200).json({
+                message: "Comment Deleted successfully"
+            });
+        };
+    } catch(err) {
+        res.status(403).json({
+            message: "Failed Deleted Comment"
+        });
+    }
+} 
+
+module.exports = {
+    GetComment,
+    Commentcreate,
+    Commentupdate,
+    Commentdelete
+}
