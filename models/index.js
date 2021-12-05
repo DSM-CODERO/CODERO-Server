b6c6c342c56e22c8bd1e74a6fe021a8e88654576
skmn3 <sangjin170202@naver.com>
2021-12-05 09:17:07 +0900

    [UPDATE] Index Model

--- models/index.js
+++ models/index.js
@@ -20,19 +20,34 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+<<<<<<< Updated upstream
 db.User = User;
 db.Comment = Comment;
 db.Board = Board;
 db.Like = Like;
+=======
+db.User = require("./user")(sequelize, Sequelize);
+db.Comment = require("./comment")(sequelize, Sequelize);
+db.Board = require("./board")(sequelize, Sequelize);
+db.Like = require("./like")(sequelize, Sequelize);
+db.RefreshToken = require("./refresh_token")(sequelize, Sequelize);
+db.Role = require("./role")(sequelize, Sequelize);
+db.userRole = require("./user_roles")(sequelize, Sequelize);
+>>>>>>> Stashed changes
 
 User.init(sequelize);
 Comment.init(sequelize);
 Like.init(sequelize);
 Board.init(sequelize);
 
+<<<<<<< Updated upstream
 User.associate(db);
 Comment.associate(db);
 Like.associate(db);
 Board.associate(db);
+=======
+db.User.hasMany(db.RefreshToken, { foreignKey: "user_id", tatgetKey: "user_id"});
+db.RefreshToken.belongsTo(db.User, { foreignKey: "user_id"});
+>>>>>>> Stashed changes
 
 module.exports = db;
