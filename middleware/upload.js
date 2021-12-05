2a2f4bf6dbb32763983c283c30e74649bd3a7b67
skmn3 <sangjin170202@naver.com>
2021-12-05 18:39:37 +0900

    [CREATE] Upload middleware

---
+++ middleware/upload.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+aws.config.loadFromPath(__dirname + '/../config/s3.json');
+
+const s3 = new aws.S3();
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'elasticbeanstalk-us-east-2-079870045884',
+        acl: 'public-read',
+        filefilter: (req, file, cb) => {
+            const ext = path.extname(file.originalname);
+            if(ext !== "jpeg" || "jpg" || "png" || "gif" || "pdf" || "svg") {
+                return cb(null, false)
+            }
+            cb(null, true)
+        },
+        key: (req, file, cb) => {
+            cb(null, Date.now() + '.' + file.originalname.split('.').pop()); // 이름 설정
+        },
+        limit: {
+            filesize: 1024 * 1024 * 20
+          },
+    }),
+});
+module.exports = upload;
