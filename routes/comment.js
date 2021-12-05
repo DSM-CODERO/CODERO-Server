59d2a39b5d7126b8004c5d18a4b442ac47fa1165
skmn3 <sangjin170202@naver.com>
2021-12-05 20:52:04 +0900

    [UPDATE] Comment Route

--- routes/comment.js
+++ routes/comment.js
@@ -1,29 +1,11 @@
 const router = require("express")();
-const multer = require("multer")
 const commentCtrl = require("../controllers/comment.controller");
 const verifyToken = require("../middleware/token");
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname)
-  },
-  filefilter: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    if(ext !== "jpeg" || "jpg" || "png" || "gif" || "pdf" || "svg") {
-        return cb(null, false)
-    }
-    cb(null, true)
-  },
-  limit: {
-    filesize: 1024 * 1024 * 20
-  },
-})
-
-const upload = multer({storage: storage}).array("image", 12);
+const upload = require("../middleware/upload")
 
 router.get('/', verifyToken, commentCtrl.GetComment);
-router.post("/", verifyToken, upload, commentCtrl.Commentcreate);
+router.post("/", verifyToken, upload.array('Image', 14) , commentCtrl.Commentcreate);
 router.patch('/:id', verifyToken, commentCtrl.Commentupdate);
 router.delete('/:id', verifyToken, commentCtrl.Commentdelete);
 
