6ce89785d5870b4e0cb83f628b15426e66cd7d1d
skmn3 <sangjin170202@naver.com>
2021-12-05 20:36:52 +0900

    [UPDATE] Upload Middleware

--- middleware/upload.js
+++ middleware/upload.js
@@ -7,7 +7,7 @@ const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'elasticbeanstalk-us-east-2-079870045884',
+        bucket: 'elasticbeanstalk-ap-northeast-9-820927005893',
         acl: 'public-read-write',
         key: (req, file, cb) => {
             cb(null, Math.floor(Math.random() * 1000).toString() + Date.now() + '.' + file.originalname.split('.').pop());
