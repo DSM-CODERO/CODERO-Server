ddd228ddf437eb765ccd22e4b4de269583abaa7d
skmn3 <sangjin170202@naver.com>
2021-12-05 09:50:25 +0900

    [UPDATE] user_role Model

--- models/user_roles.js
+++ models/user_roles.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-const Sequelize = require('sequelize');
-
-module.exports =(sequelize, Datatypes) => {
-  return sequelize.define("like", {
-    users_id: {
-      type: Datatypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-    },
-    roles_id: { 
-      type: Datatypes.INTEGER,
-      allowNull: false,
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    modelName: 'User_roles',
-    tableName: 'user_roles',
-    paranoid: false,
-    charset: 'utf8',
-    collate: 'utf8_general_ci',
-  })
-}
-
-=======
 module.exports =(sequelize, Datatypes) => { 
     return sequelize.define("like", { 
       users_id: { 
@@ -46,5 +19,4 @@ module.exports =(sequelize, Datatypes) => {
       charset: 'utf8', 
       collate: 'utf8_general_ci', 
     }) 
-  } 
->>>>>>> develop
+} 
