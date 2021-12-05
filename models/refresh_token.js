f5387f82eb26748f111480e509f496ae00eea088
skmn3 <sangjin170202@naver.com>
2021-12-05 09:50:01 +0900

    [UPDATE] refresh_token Model

--- models/refresh_token.js
+++ models/refresh_token.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-const Sequelize = require('sequelize');
-
-module.exports =(sequelize, Datatypes) => {
-  return sequelize.define("refresh_token", {
-    id: {
-        type: Datatypes.BIGINT,
-        primaryKey: true,
-    },
-    user_id: {
-        type: Datatypes.INTEGER,
-        allowNull: false,
-    },
-    token: { 
-        type: Datatypes.STRING,
-        allowNull: false,
-    },
-    expriyDate: {
-        type: Datatypes.DATE,
-        allowNull: false,
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    modelName: 'Refresh_token',
-    tableName: 'refresh_token',
-    paranoid: false,
-    charset: 'utf8',
-    collate: 'utf8_general_ci',
-  })
-=======
 module.exports =(sequelize, Datatypes) => { 
   return sequelize.define("refresh_token", { 
     id: { 
@@ -60,5 +28,4 @@ module.exports =(sequelize, Datatypes) => {
     charset: 'utf8', 
     collate: 'utf8_general_ci', 
   }) 
->>>>>>> develop
 }
