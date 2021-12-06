f69bfd0293cab0baae593af4a45a0cb89446c98f
yebin <yebin5127@gmail.com>
2021-12-07 08:28:24 +0900

    [FIX] readPost routes

--- routes/readPost.js
+++ routes/readPost.js
@@ -1,9 +1,9 @@
 const router = require("express")();
 const controller = require("../controllers/readPost");
-const verifyToken = require("../middleware/token");
 
+router.get("/allview", controller.ReadAllView);
+router.get("/filed/:filed", controller.ReadFiledPost);
 router.get("/:board_id", controller.ReadOnePost);
-router.get("/", controller.ReadAllView);
 router.get("/nickname/:nickname", controller.ReadAllPost);
 
 module.exports = router;
