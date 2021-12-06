966a4377a255d61d80a7f7545a19447328699a53
yebin <yebin5127@gmail.com>
2021-12-06 09:24:58 +0900

    [UDPATE] role model

---
+++ models/role 2.js
@@ -0,0 +1,22 @@
+module.exports =(sequelize, Datatypes) => { 
+  return sequelize.define("role", { 
+    id: { 
+      type: Datatypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+    }, 
+    name: {  
+      type: Datatypes.STRING, 
+      allowNull: false, 
+    } 
+  }, 
+  { 
+    sequelize, 
+    timestamps: false, 
+    modelName: 'Role', 
+    tableName: 'roles', 
+    paranoid: false, 
+    charset: 'utf8', 
+    collate: 'utf8_general_ci', 
+  }) 
+} 
---
+++ models/user_roles 2.js
@@ -0,0 +1,22 @@
+module.exports =(sequelize, Datatypes) => { 
+    return sequelize.define("like", { 
+      users_id: { 
+        type: Datatypes.INTEGER, 
+        allowNull: false, 
+        primaryKey: true, 
+      }, 
+      roles_id: {  
+        type: Datatypes.INTEGER, 
+        allowNull: false, 
+      } 
+    }, 
+    { 
+      sequelize, 
+      timestamps: false, 
+      modelName: 'User_roles', 
+      tableName: 'user_roles', 
+      paranoid: false, 
+      charset: 'utf8', 
+      collate: 'utf8_general_ci', 
+    }) 
+  } 
