2d38d0535a2d60a13cea620dddb31e18926f6c63
yebin <yebin5127@gmail.com>
2021-12-06 17:03:50 +0900

    [FIX] upload middleware

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,15 +2,15 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
 aws.config.loadFromPath(__dirname + '/../config/s3.json');
-
 const s3 = new aws.S3();
+
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'elasticbeanstalk-ap-northeast-9-820927005893',
-        acl: 'public-read-write',
+        bucket: 'codero',
+        acl: 'public-read',
         key: function(req, file, cb) {
-            cb(null, Math.floor(Math.random() * 1000).toString() + Date.now() + '.' + file.originalname.split('.').pop());
+            cb(null, Date.now() + '.' + file.originalname.split('.').pop());
         },
         limit: {
             fileSize: 1024 * 1024 * 20
