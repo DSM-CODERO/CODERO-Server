935f5322af7d0651817ff82ce251e4cb2abd34ef
skmn3 <sangjin170202@naver.com>
2021-12-07 20:58:07 +0900

    [FIX] Index Model

--- models/index.js
+++ models/index.js
@@ -2,15 +2,13 @@
 
 const Sequelize = require('sequelize');
 
-const env = process.env.NODE_ENV || 'development';
-const config = require("../config/config.json")[env];
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config");
+
 const db = {};
 
 const sequelize = new Sequelize(
-    config.database, 
-    config.username, 
-    config.password, 
-    config
+    config,
 );
 
 db.sequelize = sequelize;
@@ -25,9 +23,6 @@ db.User = require("./user")(sequelize, Sequelize);
 db.Comment = require("./comment")(sequelize, Sequelize);
 db.Board = require("./board")(sequelize, Sequelize);
 db.Like = require("./like")(sequelize, Sequelize);
-db.RefreshToken = require("./refresh_token")(sequelize, Sequelize);
-db.Role = require("./role")(sequelize, Sequelize);
-db.userRole = require("./user_role")(sequelize, Sequelize);
 
 db.User.hasMany(db.Comment, { foreignKey: "user_id", tatgetKey: "user_id"});
 db.Comment.belongsTo(db.User, { foreignKey: "user_id"});
@@ -40,8 +35,5 @@ db.Comment.belongsTo(db.Board, { foreignKey: "board_id"});
 
 db.Board.hasMany(db.Like, { foreignKey: "board_id", tatgetKey: "board_id"});
 db.Like.belongsTo(db.Board, { foreignKey: "board_id"});
-
-db.User.hasMany(db.RefreshToken, { foreignKey: "user_id", tatgetKey: "user_id"});
-db.RefreshToken.belongsTo(db.User, { foreignKey: "user_id"});
 
 module.exports = db;
