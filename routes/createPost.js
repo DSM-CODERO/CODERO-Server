8255951c8a53dad47909a15440eeb812ecec4b56
skmn3 <sangjin170202@naver.com>
2021-12-07 21:23:40 +0900

    [DELETE] CreatePost Route
    주석 지우기

--- routes/createPost.js
+++ routes/createPost.js
@@ -1,21 +1,8 @@
 const router = require("express")();
 const controller = require("../controllers/createPost");
 const verifyToken = require("../middleware/token");
-const path = require("path");
-const upload = require("../middleware/upload");
-const multer = require("multer");
 
-// const storage = multer.diskStorage({
-//     destination: "uploads/",    
-//     filename: (req, file, cb) => { 
-//         cb(null, Date.now() + "-" + file.originalname);   
-//     }, 
-   
-// });
-         
-// const upload = multer({
-//     storage: storage,
-// });
+const upload = require("../middleware/upload");
 
 router.post("/", verifyToken, upload.single('image'), controller.createPost);
 
