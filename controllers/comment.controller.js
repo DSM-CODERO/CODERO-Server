917587448148d61e695b6696cc728d61a2027275
yebin <yebin5127@gmail.com>
2021-12-07 11:22:31 +0900

    [FIX] comment controller

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -24,51 +24,70 @@ const Commentcreate = async(req, res) => {
     const nickName = req.decoded.nickname;
     const Image = req.file;
     const { context } = req.body;
-
     
-    try{
-        await Comment.create({
-            user_id : UserID,
-            nickname : nickName,
-            context : context,
-            board_id : BoardId,
-            image : Image.location
-        })
-        res.status(200).json({ 
-            message: "댓글 작성 성공"
-        });
-    } catch(err){
-        res.status(404).json({
-            message: "댓글 작성 실패"
-        });
-        console.error(err)
-    }
-}
+    if(Image === undefined){
+        try{
+            await Comment.create({
+                user_id: UserID,
+                nickname : nickName,
+                context: context,
+                board_id: BoardId
+            })
+            res.status(200).json({
+                messgae: "댓글 작성 성공"
+            });
+        } catch(err) {
+            res.status(403).json({
+                message : "댓글 작성 실패"
+            })
+            console.error(err);
+        }} 
+        else{
+            try{
+            await Comment.create({
+                user_id : UserID,
+                nickname : nickName,
+                context : context,
+                board_id : BoardId,
+                image : Image.location
+            })
+            res.status(200).json({ 
+                message: "댓글 작성 성공"
+            });
+        } catch(err){
+            res.status(403).json({
+                message: "댓글 작성 실패"
+            });
+            console.error(err);
+        }
+    }};
 
 
 const Commentupdate = async(req, res) => {
     const UserID = req.decoded.user_id;
     const CommentID = req.params.comment_id;
-    const { context } = req.body;
+    const { Context } = req.body;
 
+    console.log(Context);
     try{
-        const result = await Comment.findOne({
+        const comment = await Comment.findOne({
             where : {
                 comment_id : CommentID
             },
         });
-        if(result.user_id !== UserID) {
+        
+        console.log(comment);
+
+        if(comment.user_id !== UserID) {
             res.status(400).json({
                 message: "본인 댓글만 수정 가능"
             });
-        } else{
-            await result.update({
-                context : context,
-            });
+        } else await comment.update({
+            context : Context
+            })
             res.status(200).json({
                 message: "댓글 수정 성공"
             });
-        } 
     } catch(err) {
         res.status(403).json({
             message: "댓글 수정 실패"
@@ -109,4 +128,4 @@ module.exports = {
     Commentcreate,
     Commentupdate,
     Commentdelete
-}
+};
