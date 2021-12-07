f4101a8fb7e530beb8b47a129deacba8197ed4dd
skmn3 <sangjin170202@naver.com>
2021-12-07 22:55:13 +0900

    [FIX] Comment Model

--- models/comment.js
+++ models/comment.js
@@ -37,7 +37,7 @@ module.exports = (sequelize, Datatypes) => {
     sequelize,
     timestamps: false,
     modelName: 'Comment',
-    tableName: 'comments',
+    tableName: 'comment',
     paranoid: false,
     charset: 'utf8mb4',
     collate: 'utf8mb4_general_ci',
