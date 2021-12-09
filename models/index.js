2114a247e179b5c9f4d74ee540958b85dbfd1fc9
skmn3 <sangjin170202@naver.com>
2021-12-09 12:30:07 +0900

    [FIX] Index Model

--- models/index.js
+++ models/index.js
@@ -24,11 +24,11 @@ db.Comment = require("./comment")(sequelize, Sequelize);
 db.Board = require("./board")(sequelize, Sequelize);
 db.Like = require("./like")(sequelize, Sequelize);
 
-db.User.hasMany(db.Comment, { foreignKey: "user_id", targetKey: "id"});
-db.Comment.belongsTo(db.User, { foreignKey: "user_id"});
+db.User.hasMany(db.Comment, { foreignKey: "id", targetKey: "id"});
+db.Comment.belongsTo(db.User, { foreignKey: "id"});
 
 db.User.hasMany(db.Like, { foreignKey: "user_id", targetKey: "id"});
-db.Like.belongsTo(db.User, { foreignKey: "user_id"});
+db.Like.belongsTo(db.User, { foreignKey: "id"});
 
 db.Board.hasMany(db.Comment, { foreignKey: "board_id", targetKey: "board_id"});
 db.Comment.belongsTo(db.Board, { foreignKey: "board_id"});
