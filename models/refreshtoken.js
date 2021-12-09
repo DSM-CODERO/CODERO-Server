aae8769d2b59b02abfad16b7c27c45febd1303ad
skmn3 <sangjin170202@naver.com>
2021-12-09 13:25:50 +0900

    [CREATE] refreshtoken

---
+++ models/refreshtoken.js
@@ -0,0 +1,31 @@
+module.exports =(sequelize, Datatypes) => {  
+    return sequelize.define("refreshtoken", {  
+      id: {  
+          type: Datatypes.BIGINT, 
+          allowNull: false, 
+          autoIncrement: true, 
+          primaryKey: true,  
+      }, 
+      user_id: {  
+          type: Datatypes.INTEGER,  
+          allowNull: false,  
+      },  
+      token: {   
+          type: Datatypes.STRING,  
+          allowNull: false,  
+      },  
+      expriyDate: {  
+          type: Datatypes.DATE,  
+          allowNull: false,  
+      }  
+    },  
+    {  
+      sequelize,  
+      timestamps: false,  
+      modelName: 'refreshtoken',  
+      tableName: 'refreshtoken',  
+      paranoid: false,  
+      charset: 'utf8',  
+      collate: 'utf8_general_ci',  
+    })  
+  }
