8df5ac01a8c6537708739802675f0f84f4cc0887
yebin <yebin5127@gmail.com>
2021-12-14 16:18:36 +0900

    [ADD] user routes
    user 정보 보기 기능 routes 추가

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const router = require("express")();
 const controller = require("../controllers/user");
+const verifyToken = require("../middleware/token");
 
 router.post("/", controller.sign_up);
 router.post("/login", controller.login);
+router.get("/mypage",verifyToken, controller.viewMyPage);
 
 module.exports = router;
