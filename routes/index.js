7aaae4a7ef498dc95ae26a10b25bbb22daf3bf92
skmn3 <sangjin170202@naver.com>
2021-12-16 06:35:17 +0900

    [DELETE] 불필요한 코드 삭제
    불필요한 코드 삭제

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,12 @@ const like = require("./like");
 const user = require("./user");
 const comment = require("./comment");
 
+router.use("/user", user);
 router.use("/board", createPost);
 router.use("/board", deletePost);
 router.use("/board", readPost);
 router.use("/board", updatePost);
-
 router.use("/board", like);
-
-router.use("/user", user);
-
 router.use("/board", comment)
 
 module.exports = router;
