e2e8976db8d2cf5f19b992002092211b62851b1d
skmn3 <sangjin170202@naver.com>
2021-12-16 06:33:39 +0900

    [UPDATE] single -> array로 변경
    S3 사진 여러 개 추가할 수 있게 array로 변경

--- routes/comment.js
+++ routes/comment.js
@@ -1,13 +1,10 @@
 const router = require("express")();
 const controller = require("../controllers/comment.controller");
 const verifyToken = require("../middleware/token");
-
 const upload = require("../middleware/upload");
 
-
-
 router.get('/:board_id/comment', controller.GetComment);
-router.post("/:board_id/comment", verifyToken, upload.single('image'), controller.Commentcreate);
+router.post("/:board_id/comment", verifyToken, upload.array('image'), controller.Commentcreate);
 router.patch('/:board_id/comment/:comment_id', verifyToken, controller.Commentupdate);
 router.delete('/:board_id/comment/:comment_id', verifyToken, controller.Commentdelete);
 
