f04f2ec152b3a7d6139f20dba687f538d8c5d846
skmn3 <sangjin170202@naver.com>
2021-12-31 13:02:52 +0900

    [#27] chore: eslint와 prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,7 @@ module.exports = {
     'no-restricted-syntax': [
       'error',
       {
-        selector:
-          "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
         message: 'Unexpected property on console object was called',
       },
     ],
